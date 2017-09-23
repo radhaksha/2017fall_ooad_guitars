be921eba83e1d0d9fd78d058e3b65cd9be769073
omontalv <omontalv@fitchburgstate.edu>
2017-09-23 17:20:02 -0400

    Raghuram Rangaraju changes.

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,43 +1,106 @@
+
+/**
+ * This class implements the properties and behaviour of Guitar.
+ *
+ * @author raghuram
+ */
 public class Guitar {
 
-  private String serialNumber, builder, model, type, backWood, topWood;
-  private double price;
+    private String serialNumber, builder, model, type, backWood, topWood;
+    private double price;
 
-  public Guitar(String serialNumber, double price,
-                String builder, String model, String type,
-                String backWood, String topWood) {
-    this.serialNumber = serialNumber;
-    this.price = price;
-    this.builder = builder;
-    this.model = model;
-    this.type = type;
-    this.backWood = backWood;
-    this.topWood = topWood;
-  }
+    /**
+     * Constructor for the class Guitar with the following arguments
+     *
+     * @param serialNumber
+     * @param price
+     * @param builder
+     * @param model
+     * @param type
+     * @param backWood
+     * @param topWood
+     */
+    public Guitar(String serialNumber, double price,
+            String builder, String model, String type,
+            String backWood, String topWood) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+    }
 
-  public String getSerialNumber() {
-    return serialNumber;
-  }
+    /**
+     * Function returns the serial number of guitar
+     *
+     * @return serialNumber
+     */
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
-  public double getPrice() {
-    return price;
-  }
-  public void setPrice(float newPrice) {
-    this.price = newPrice;
-  }
-  public String getBuilder() {
-    return builder;
-  }
-  public String getModel() {
-    return model;
-  }
-  public String getType() {
-    return type;
-  }
-  public String getBackWood() {
-    return backWood;
-  }
-  public String getTopWood() {
-    return topWood;
-  }
+    /**
+     * Function returns the price of guitar
+     *
+     * @return price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * Function sets the new price
+     *
+     * @param newPrice set the new price
+     */
+    public void setPrice(float newPrice) {
+        this.price = newPrice;
+    }
+
+    /**
+     * The function returns builder information of guitar
+     *
+     * @return builder
+     */
+    public String getBuilder() {
+        return builder;
+    }
+
+    /**
+     * The function returns model of guitar
+     *
+     * @return model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * The function returns type of guitar
+     *
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * The function returns Backwood
+     *
+     * @return Backwood
+     */
+    public String getBackWood() {
+        return backWood;
+    }
+
+    /**
+     * The function returns the topwood
+     *
+     * @return topWood
+     */
+    public String getTopWood() {
+        return topWood;
+    }
 }
--- src/Inventory.java
+++ src/Inventory.java
@@ -1,57 +1,98 @@
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class implements properties and behavior of inventory
+ *
+ * @author raghuram
+ */
 public class Inventory {
-  private List guitars;
 
-  public Inventory() {
-    guitars = new LinkedList();
-  }
+    private List<Guitar> guitars;
 
-  public void addGuitar(String serialNumber, double price,
-                        String builder, String model,
-                        String type, String backWood, String topWood) {
-    Guitar guitar = new Guitar(serialNumber, price, builder,
-                               model, type, backWood, topWood);
-    guitars.add(guitar);
-  }
-  public Guitar getGuitar(String serialNumber) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      if (guitar.getSerialNumber().equals(serialNumber)) {
-        return guitar;
-      }
+    /**
+     * This is the Constructor for the class Inventory and initialise guitars linkedlist
+     */
+    public Inventory() {
+        guitars = new LinkedList();
     }
-    return null;
-  }
-  public Guitar search(Guitar searchGuitar) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      // Ignore serial number since that's unique
-      // Ignore price since that's unique
-      String builder = searchGuitar.getBuilder();
-      if ((builder != null) && (!builder.equals("")) &&
-          (!builder.equals(guitar.getBuilder())))
-        continue;
-      String model = searchGuitar.getModel();
-      if ((model != null) && (!model.equals("")) &&
-          (!model.equals(guitar.getModel())))
-        continue;
-      String type = searchGuitar.getType();
-      if ((type != null) && (!searchGuitar.equals("")) &&
-          (!type.equals(guitar.getType())))
-        continue;
-      String backWood = searchGuitar.getBackWood();
-      if ((backWood != null) && (!backWood.equals("")) &&
-          (!backWood.equals(guitar.getBackWood())))
-        continue;
-      String topWood = searchGuitar.getTopWood();
-      if ((topWood != null) && (!topWood.equals("")) &&
-          (!topWood.equals(guitar.getTopWood())))
-        continue;
-      return guitar;
+
+    /**
+     * The function adds up new guitar into guitars list
+     *
+     * @param serialNumber
+     * @param price
+     * @param builder
+     * @param model
+     * @param type
+     * @param backWood
+     * @param topWood
+     */
+    public void addGuitar(String serialNumber, double price,
+            String builder, String model,
+            String type, String backWood, String topWood) {
+        Guitar guitar = new Guitar(serialNumber, price, builder,
+                model, type, backWood, topWood);
+        guitars.add(guitar);
     }
-    return null;
-  }
+
+    /**
+     * The function gets the serialNumber and compare with guitar list serial
+     * number if there is any match returns guitar object else returns null
+     *
+     * @param serialNumber
+     * @return guitar object
+     */
+    public Guitar getGuitar(String serialNumber) {
+        for (Iterator i = guitars.iterator(); i.hasNext();) {
+            Guitar guitar = (Guitar) i.next();
+            if (guitar.getSerialNumber().equals(serialNumber)) {
+                return guitar;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * The function gets searchGuitar object
+     *
+     * @param searchGuitar
+     * @return guitar object or null
+     */
+    public Guitar search(Guitar searchGuitar) {
+        for (Iterator i = guitars.iterator(); i.hasNext();) {
+            Guitar guitar = (Guitar) i.next();
+            // Ignore serial number since that's unique
+            // Ignore price since that's unique
+            String builder = searchGuitar.getBuilder();
+            if ((builder != null) && (!builder.equals(""))
+                    && (!builder.equals(guitar.getBuilder()))) {
+                continue;
+            }
+            String model = searchGuitar.getModel();
+            if ((model != null) && (!model.equals(""))
+                    && (!model.equals(guitar.getModel()))) {
+                continue;
+            }
+            String type = searchGuitar.getType();
+            if ((type != null) && (!searchGuitar.equals(""))
+                    && (!type.equals(guitar.getType()))) {
+                continue;
+            }
+            String backWood = searchGuitar.getBackWood();
+            if ((backWood != null) && (!backWood.equals(""))
+                    && (!backWood.equals(guitar.getBackWood()))) {
+                continue;
+            }
+            String topWood = searchGuitar.getTopWood();
+            if ((topWood != null) && (!topWood.equals(""))
+                    && (!topWood.equals(guitar.getTopWood()))) {
+                continue;
+            }
+            return guitar;
+        }
+        return null;
+    }
 }
